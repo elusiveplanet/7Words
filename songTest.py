5fd156bcf5074e515114370426385af520e13eb7
Kurama <renderosalexis17@gmail.com>
2019-03-24 23:53:07 -0500

    Prelim code for grabbing playlists from user, gonna work on grabbing track list from selected playlist next. -Alexis

--- songTest.py
+++ songTest.py
@@ -48,3 +48,13 @@ for x in naughtyWords:
 	else:
         	print ("Target word " + naughtyWords[i] + " located!")
 	i += 1
+
+
+playlists = sp.user_playlists('renderos17')
+while playlists:
+    for i, playlist in enumerate(playlists['items']):
+        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+    if playlists['next']:
+        playlists = sp.next(playlists)
+    else:
+        playlists = None
